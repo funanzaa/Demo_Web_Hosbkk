9e224d7b14466bac3590218a6dea05a9d91bd569
funanzaa <eakachai_golf@hotmail.com>
2020-07-02 12:01:15 +0700

    Add template Home pages

--- Web_Hosbkk/hosbkk_app/urls.py
+++ Web_Hosbkk/hosbkk_app/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    path('', showDemoPage , name = 'showDemoPage'),
+    path('', HomePage , name = 'home-page'),
 ]
--- Web_Hosbkk/hosbkk_app/views.py
+++ Web_Hosbkk/hosbkk_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
-def showDemoPage(request):
-    return render(request, 'demo.html')
+def HomePage(request):
+    return render(request, 'home.html')
